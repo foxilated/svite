021c6a3ebe791a70b9880897a080c2a377e2f3de
Khaled Waleed <foxilated@pm.me>
2022-02-28 22:03:30 +0300

    changed svelte template

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import Router from 'svelte-spa-router';
-  import routes from 'plugins/router';
+  import routes from './plugins/router';
 </script>
 
 <Router {routes} />
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,17 +1,69 @@
-<div>
-  <h1>hello friend.</h1>
-  <span class="i-ri:robot-fill" />
+<script>
+  import Header from 'lib/Header';
+  import Button from 'lib/Button';
+</script>
+
+<div class="container">
+  <Header />
+  <div class="main">
+    <h1>The<br />Post<br />Project</h1>
+    <p class="top">opportunities at your fingertips</p>
+    <p class="bot">at your fingertips</p>
+  </div>
+  <div class="button"><Button /></div>
+  <div class="squared">
+    <div class="square" />
+  </div>
 </div>
 
-<style lang="scss">
-  div {
-    height: 100vh;
+<style>
+  .squared {
+    position: fixed;
     display: flex;
-    justify-content: center;
     align-items: center;
-    h1 {
-      color: var(--black);
-      font-weight: 300;
+    justify-content: left;
+  }
+  .square {
+    height: 30vmin;
+    width: 30vmin;
+    border: 0.1vmin solid var(--white);
+    background: transparent;
+    margin-bottom: -60vmin;
+    margin-left: -15vmin;
+  }
+  .container {
+    background: var(--blue);
+    height: 100vh;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+  }
+  .button{
+    margin-left: 50%;
+  }
+  h1 {
+    font-size: 12vmin;
+    text-transform: uppercase;
+    color: var(--white);
+    padding: 8% 0 0 30%;
+  }
+  p {
+    color: var(--black);
+    font-size: 4vmin;
+    font-weight: 300;
+    text-transform: uppercase;
+    letter-spacing: 0.5ch;
+  }
+  .top {
+    transform: rotate(90deg);
+  }
+  .bot {
+    margin: 0;
+  }
+  @media all and (max-width: 1000px) {
+    .main {
+      margin-top: 50%;
     }
   }
 </style>
